8fcc60264db0895aa748c5d1e901b87be29f783b
neomy <neom21leva@gmail.com>
2018-11-15 18:46:11 +0200

    ll

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask,render_template, request
 from flask import render_template
-from database import get_all_cats, find_cat
+from database import get_all_cats, find_cat, create_cat
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'YOUR-VERY-SECRET-SHHH'
@@ -15,6 +15,15 @@ def view_details(number):
 	cat = find_cat(number)
 	return render_template("cat.html",cat= cat)
 
+@app.route('/create', methods=['GET','POST'])
+def make_cat():
+	if request.method =="GET":
+		return render_template("addcat.html")
+	else:
+		name = request.form['firstname']
+		create_cat(name)
+		cats = get_all_cats()
+		return render_template('home.html', cats=cats)
 
 if __name__ == '__main__':
    app.run(debug = True)
